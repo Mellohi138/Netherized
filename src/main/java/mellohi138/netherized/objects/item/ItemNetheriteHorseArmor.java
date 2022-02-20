11646ccb83ef9cf73fe45921a26bdbeb722c1833
Mellohi <mallohi138@gmail.com>
2022-02-20 23:23:20 +0200

    Update ItemNetheriteHorseArmor.java

--- src/main/java/mellohi138/netherized/objects/item/ItemNetheriteHorseArmor.java
+++ src/main/java/mellohi138/netherized/objects/item/ItemNetheriteHorseArmor.java
@@ -3,7 +3,6 @@ package mellohi138.netherized.objects.item;
 import java.util.UUID;
 
 import mellohi138.netherized.Netherized;
-import mellohi138.netherized.init.NetherizedMaterials;
 import mellohi138.netherized.util.config.NetherizedConfig;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLiving;
@@ -16,19 +15,19 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class ItemNetheriteHorseArmor extends Item {
-	/*
 	protected static final UUID KNOCKBACK_RESISTANCE_MODIFIER = UUID.fromString("ba912d2b-9413-3171-be0e-5b384abadf2a");
 	public static final float KNOCKBACK_RESISTANCE_BONUS = NetherizedConfig.netheriteHorseArmorKnockbackResistance;
-	*/
+	private final HorseArmorType armorType;
 	
-	public ItemNetheriteHorseArmor(String name, CreativeTabs tab) {
+	public ItemNetheriteHorseArmor(String name, HorseArmorType armorType, CreativeTabs tab) {
 		this.setTranslationKey(name);
 		this.setRegistryName(Netherized.MODID, name);
 		this.setCreativeTab(tab);
 		this.setMaxStackSize(1);
+		
+		this.armorType = armorType;
 	}
     
-	/*
     @Override
     public void onHorseArmorTick(World world, EntityLiving horse, ItemStack armor) {
     	IAttributeInstance attribute = horse.getEntityAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE);
@@ -36,16 +35,11 @@ public class ItemNetheriteHorseArmor extends Item {
         	if(attribute.getModifier(KNOCKBACK_RESISTANCE_MODIFIER) == null) {
         		attribute.applyModifier(new AttributeModifier(KNOCKBACK_RESISTANCE_MODIFIER, "knockback_resistance_bonus", KNOCKBACK_RESISTANCE_BONUS, 0).setSaved(false));
         	}
-    	} else if(armor.getItem() != this) {
-        	if(attribute.getModifier(KNOCKBACK_RESISTANCE_MODIFIER) != null) {
-        		attribute.removeModifier(KNOCKBACK_RESISTANCE_MODIFIER);
-        	}
     	}
     }
-    */
 	
 	@Override
     public HorseArmorType getHorseArmorType(ItemStack stack) {
-        return NetherizedMaterials.NETHERITE_HORSE_ARMOR;
+        return this.armorType;
     }
 }
