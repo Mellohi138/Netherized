650c25a5dcf537c174d414210f7acc1a91eef90d
Mellohi <mallohi138@gmail.com>
2022-02-25 11:51:48 +0200

    Update EnumNetherizedParticles.java

--- src/main/java/mellohi138/netherized/enums/EnumNetherizedParticles.java
+++ src/main/java/mellohi138/netherized/enums/EnumNetherizedParticles.java
@@ -1,12 +1,5 @@
 package mellohi138.netherized.enums;
 
-import java.util.Map;
-import java.util.Set;
-
-import javax.annotation.Nullable;
-
-import com.google.common.collect.Maps;
-
 public enum EnumNetherizedParticles {
 	REVERSED_PORTAL("reversed_portal", 0),
 	CRYING_OBSIDIAN_TEAR("crying_obsidian_tear", 1),
@@ -17,16 +10,10 @@ public enum EnumNetherizedParticles {
 	
     private final String particleName;
     private final int particleID;
-    private static final Map<Integer, EnumNetherizedParticles> PARTICLES = Maps.<Integer, EnumNetherizedParticles>newHashMap();
-    private static final Map<String, EnumNetherizedParticles> BY_NAME = Maps.<String, EnumNetherizedParticles>newHashMap();
 
     private EnumNetherizedParticles(String particleNameIn, int particleIDIn) {
         this.particleName = particleNameIn;
         this.particleID = particleIDIn;
-    }
-
-    public static Set<String> getParticleNames() {
-        return BY_NAME.keySet();
     }
 
     public String getParticleName() {
@@ -35,22 +22,5 @@ public enum EnumNetherizedParticles {
 
     public int getParticleID() {
         return this.particleID;
-    }
-
-    @Nullable
-    public static EnumNetherizedParticles getParticleFromId(int particleId) {
-        return PARTICLES.get(Integer.valueOf(particleId));
-    }
-
-    @Nullable
-    public static EnumNetherizedParticles getByName(String nameIn) {
-        return BY_NAME.get(nameIn);
-    }
-
-    static {
-        for (EnumNetherizedParticles enumparticletypes : values()) {
-            PARTICLES.put(Integer.valueOf(enumparticletypes.getParticleID()), enumparticletypes);
-            BY_NAME.put(enumparticletypes.getParticleName(), enumparticletypes);
-        }
     }
 }
